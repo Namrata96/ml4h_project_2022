3cf62f0098aaf173ee83c078985a927a8cd999ad
Namrata Mukhija <nm3571@log-2.hpc.nyu.edu>
2022-12-09 00:08:46 -0500

    Added perplexity metric.

--- metrics/perplexity.py
+++ metrics/perplexity.py
@@ -3,4 +3,4 @@ from evaluate import load
 def mean_perplexity(predictions):
     perplexity = load("perplexity", module_type="metric")
     results = perplexity.compute(predictions=predictions, model_id='gpt2')
-    return results['mean_perplexity']
+    return round(results['mean_perplexity'],3)
