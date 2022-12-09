27ad0eabf5d9d15313653d6b99e45a66299eb345
Namrata Mukhija <nm3571@log-2.hpc.nyu.edu>
2022-12-08 23:57:07 -0500

    Added BLEU and ROUGE metrics.

---
+++ metrics/rouge.py
@@ -0,0 +1,6 @@
+import evaluate
+
+def rouge_score(predictions, actuals)
+    rouge = evaluate.load('rouge')
+    results = rouge.compute(predictions=predictions, references=references)
+    return results # Returns: {'rouge1': 1.0, 'rouge2': 1.0, 'rougeL': 1.0, 'rougeLsum': 1.0}
